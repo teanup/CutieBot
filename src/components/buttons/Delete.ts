a1d9d63a50d73a6623ce36f2f6a8ea04e5dc0c55
peanut <antonin.frey@icloud.com>
2023-04-19 20:53:33 +0200

    [feat] delete confirmation button

--- src/components/buttons/Delete.ts
+++ src/components/buttons/Delete.ts
@@ -3,7 +3,15 @@ import { Button } from "../../structures/Button";
 export default new Button({
   customId: "delete",
   run: async ({ client, interaction }) => {
-    // TODO: implement
-    await interaction.reply({ content: interaction.picMessageId, ephemeral: true})
+    const originalMessageURL = `https://discord.com/channels/${client.guildId}/${client.picChannelId}/${interaction.picMessageId}`;
+    const embedDelete = await client.getEmbed("texts.components.buttons.delete");
+    embedDelete.title = embedDelete.title
+      .replace("${fileName}", interaction.picFileName);
+    embedDelete.description = embedDelete.description
+      .replace("${originalURL}", originalMessageURL)
+    const components = await client.getComponents("delete", interaction.picMessageId);
+
+    // Ask for confirmation
+    await interaction.reply({ embeds: [embedDelete], components, ephemeral: true });
   }
 });
