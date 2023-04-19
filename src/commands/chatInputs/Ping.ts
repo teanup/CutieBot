51d27a52a9ba30e5dbd861f4e3fb6fbfe82e6711
peanut <antonin.frey@icloud.com>
2023-04-19 12:57:51 +0200

    [style] chatInputs not textInputs

--- src/commands/chatInputs/Ping.ts
+++ src/commands/chatInputs/Ping.ts
@@ -20,7 +20,7 @@ export default new ChatInputCommand({
       return ":snail:";
     };
     
-    const embed = await client.getEmbed("texts.commands.textInputs.ping");
+    const embed = await client.getEmbed("texts.commands.chatInputs.ping");
     embed.fields[0].value = embed.fields[0].value
       .replace("${latency}", latency)
       .replace("${emoji}", emojiPicker(latency));
