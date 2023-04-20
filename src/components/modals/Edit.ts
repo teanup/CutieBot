739c336224b165ae4d60538bdf24e2c85eb8d46c
peanut <antonin.frey@icloud.com>
2023-04-20 21:44:26 +0200

    [feat] edit pictures

--- src/components/modals/Edit.ts
+++ src/components/modals/Edit.ts
@@ -1,28 +1,45 @@
 import { Modal } from "../../structures/Modal";
-import { RegisterPicOptions } from "src/bin/RegisterPic";
+import { PicOptions } from "../../bin/SetPic";
+import { TextChannel } from "discord.js";
 
 export default new Modal({
   customId: "edit",
   run: async ({ client, interaction }) => {
     // Load old and new data
     const picEmbed = await client.getEmbed(interaction.picMessageId as string, client.picEmbedsDir);
-    const oldData: RegisterPicOptions = {
+    const oldData: PicOptions = {
       title: picEmbed.title || "",
       description: picEmbed.description || "",
-      date: picEmbed.timestamp || "",
+      date: picEmbed.timestamp?.split("T")[0] || "",
       location: picEmbed.author?.name || ""
-    }
-    const newData: RegisterPicOptions = {
-      title: interaction.fields.getTextInputValue("edit-title") || "",
-      description: interaction.fields.getTextInputValue("edit-description") || "",
-      date: interaction.fields.getTextInputValue("edit-date") || "",
-      location: interaction.fields.getTextInputValue("edit-location") || ""
-    }
+    };
+    const newData = {
+      title: interaction.fields.getTextInputValue("edit-title"),
+      description: interaction.fields.getTextInputValue("edit-description"),
+      date: interaction.fields.getTextInputValue("edit-date"),
+      location: interaction.fields.getTextInputValue("edit-location")
+    };
 
     // Check for changes
-    // TODO: Check for changes
+    const newOptions: PicOptions = {
+      title: newData.title === "" ? oldData.title : newData.title,
+      description: newData.description === "" ? oldData.description : newData.description,
+      date: newData.date === "" ? oldData.date : newData.date,
+      location: newData.location === "" ? oldData.location : newData.location
+    };
 
     // Update embed
-    // TODO: Update embed
+    const picChannel = await client.channels.fetch(client.picChannelId) as TextChannel;
+    const originalPicMsg = await picChannel.messages.fetch(interaction.picMessageId as string);
+    await client.setPic(interaction.picMessageId as string, interaction.picFileName as string, originalPicMsg, picEmbed, newOptions);
+
+    // Reply
+    const originalMessageURL = `https://discord.com/channels/${interaction.guildId}/${client.picChannelId}/${interaction.picMessageId}`;
+    const embedEdit = await client.getEmbed("texts.components.modals.edit");
+    (embedEdit.title as string) = (embedEdit.title as string)
+      .replace("${fileName}", interaction.picFileName as string);
+    (embedEdit.description as string) = (embedEdit.description as string)
+      .replace("${originalMessageURL}", originalMessageURL)
+      .replace("${fileName}", interaction.picFileName as string);
   }
 });
