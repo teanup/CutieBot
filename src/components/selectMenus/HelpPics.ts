30b7985b89880219b87a14be35f537d6dca18fb7
peanut <antonin.frey@icloud.com>
2023-04-23 16:23:47 +0200

    [feat] help embed message

--- src/components/selectMenus/HelpPics.ts
+++ src/components/selectMenus/HelpPics.ts
@@ -6,6 +6,18 @@ export default new SelectMenu({
     const selected = interaction.values.at(0);
 
     const embed = await client.getEmbed(`texts.components.selectMenus.helpPics.${selected}`);
+    if (embed.description) {
+      embed.description = embed.description
+        .replace(/\${picChannelId}/g, client.picChannelId);
+    }
+    if (embed.fields) {
+      embed.fields.forEach(field => {
+        if (field.value) {
+          field.value = field.value
+            .replace(/\${picChannelId}/g, client.picChannelId);
+        }
+      });
+    }
     await interaction.reply({
       embeds: [embed],
       ephemeral: true
